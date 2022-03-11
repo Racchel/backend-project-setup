1ba1a88d6f8f8309bbb545e4ec7fb429e069a34b
Racchel <racchelvelasco@gmail.com>
2022-03-11 12:29:45 -0300

    Add TS configurations and paths

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,13 @@
 import express from 'express'
+import { TestController } from '@controllers/TestController'
 
 const app = express()
 const PORT = 3333
 
 app.get('/', (req, res) => {
+   const testController = new TestController()
+   testController.teste()
+   
    return res.json({ message: 'Hello World'})
 })
 
